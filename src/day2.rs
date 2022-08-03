7f02d9321b90b0fbb2add3ee104adf98136be46d
weykon <635883092@qq.com>
2022-08-03 11:09:45 +0800

    init

---
+++ src/day2.rs
@@ -0,0 +1,7 @@
+extern crate primal;
+// extern crate lazy_static   <-第一种，是调用第三方模块，通常是rust写的
+// 第二种，用于FFI , foreign function interfaces  ，外部函数接口
+// 不懂，先不理，反正就是外部传入的库函数，有一些忽略相关？
+
+use primal::Sieve;
+
